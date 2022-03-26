1fed49e88e4218ff8542148d6aeca08308380a72
ravirasadiya <ravirasadiya1994@gmail.com>
2022-03-26 10:33:32 +0530

    correction

--- src/components/Home/FirstSec.tsx
+++ src/components/Home/FirstSec.tsx
@@ -20,53 +20,55 @@ export default function FirstSec() {
   return (
     <>
     <Element name='Introduction'>
-        <Box className='upcro_launch_main'>
-            <Box className='width_full'>
-                <SimpleGrid columns={12} columnGap={3} rowGap={2} >
-                    <GridItem colSpan={[12, 12, 12, 6]}>
-                        <Box className='launch_text_box'>
-                            <Box className='launch_text_box_inn' data-aos="fade-up" data-aos-delay="1000" data-aos-duration="800">
-                                <Heading as="h3">upCRO</Heading>
-                                <Text>
-                                    UpCRO is our most advanced UpToken ever and our first ERC-31337 token on Cronos chain.
-                                    <br/>
-                                    Paired against CRO; upCRO is the cornerstone vault of the Root Finance upToken ecosystem on Cronos.
-                                </Text>
-                                <Box className='launch_btns'>
-                                    <Button className='btn_box_shedow' as="a" href='#'>BUY NOW</Button>
-                                    <Button as="a" href='#' className='entr_a btn_box_shedow'>ENTER APP</Button>
-                                    <Button className='btn_box_shedow' as="a" href='#'>CHART</Button>
+        <Box className='for_frts_bg'>
+            <Box className='upcro_launch_main'>
+                <Box className='width_full'>
+                    <SimpleGrid columns={12} columnGap={3} rowGap={2} >
+                        <GridItem colSpan={[12, 12, 12, 6]}>
+                            <Box className='launch_text_box'>
+                                <Box className='launch_text_box_inn' data-aos="fade-up" data-aos-delay="1000" data-aos-duration="800">
+                                    <Heading as="h3">upCRO</Heading>
+                                    <Text>
+                                        UpCRO is our most advanced UpToken ever and our first ERC-31337 token on Cronos chain.
+                                        <br/>
+                                        Paired against CRO; upCRO is the cornerstone vault of the Root Finance upToken ecosystem on Cronos.
+                                    </Text>
+                                    <Box className='launch_btns'>
+                                        <Button className='btn_box_shedow' as="a" href='#'>BUY NOW</Button>
+                                        <Button as="a" href='#' className='entr_a btn_box_shedow'>ENTER APP</Button>
+                                        <Button className='btn_box_shedow' as="a" href='#'>CHART</Button>
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
-                    </GridItem>
-                    <GridItem colSpan={[12, 12, 12, 6]}>
-                        <Box className='launch_text_box' data-aos="zoom-in" data-aos-delay="500">
-                            <motion.div onMouseMove={handleMouse}
-                                style={{
-                                    display: "flex",
-                                    placeItems: "center",
-                                    placeContent: "center",
-                                    width: "100%",
-                                    perspective: 400
-                                }}>
-                                <motion.div 
-                                style={{
-                                    width: "100%",
-                                    rotateX: rotateX,
-                                    rotateY: rotateY
-                                }}>
-                                    <Box className='radial_gradiant_box'>
-                                        <Image src='img/upbnb_first_sec.png' className='upcro_first_img' />
-                                    </Box>
-                                    {/* <Image src='img/firstsection_img.jpg' className='upcro_first_img' /> */}
+                        </GridItem>
+                        <GridItem colSpan={[12, 12, 12, 6]}>
+                            <Box className='launch_text_box' data-aos="zoom-in" data-aos-delay="500">
+                                <motion.div onMouseMove={handleMouse}
+                                    style={{
+                                        display: "flex",
+                                        placeItems: "center",
+                                        placeContent: "center",
+                                        width: "100%",
+                                        perspective: 400
+                                    }}>
+                                    <motion.div 
+                                    style={{
+                                        width: "100%",
+                                        rotateX: rotateX,
+                                        rotateY: rotateY
+                                    }}>
+                                        <Box className='radial_gradiant_box'>
+                                            <Image src='img/first_clas_img_one.svg' className='upcro_first_img' />
+                                        </Box>
+                                    </motion.div>
                                 </motion.div>
-                            </motion.div>
-                        </Box>
-                    </GridItem>
-                </SimpleGrid>
+                            </Box>
+                        </GridItem>
+                    </SimpleGrid>
+                </Box>
             </Box>
         </Box>
+        
     </Element>
     </>
   )
