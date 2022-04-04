6315c27808bb851c10abdf270b9378fc2dab5095
Billy Andreas <billyfebram@gmail.com>
2022-04-04 13:41:56 +0700

    Update swap

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,35 +1,118 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Container, Heading, Text, Button, Image } from '@chakra-ui/react'
 import LayoutTwo from './LayoutTwo'
 import ReactSelect from 'react-select';
+import { SwapService } from '../services/SwapService';
+import { useWeb3React } from '@web3-react/core';
+import { escapeRegExp, shortenAddress } from '../utils';
+import { BASE_ADDRESS, BASE_DECIMALS, ROOTED_ADDRESS, STAKING_ADDRESS } from '../constants';
+import { getDisplayBalance } from '../utils/formatBalance';
+import BigNumber from 'bignumber.js';
+import { TokenService } from '../services/TokenServices';
+import { ethers } from "ethers"
 
 const currency = [
     {
-        label: 'upCRO',
+        label: 'CRO',
         value: 0,
-        image: '/img/upcro_coin_ic.svg',
-    },
+        image: '/img/upcro_coin_ic.svg'
+    }
+];
+const currencytwo = [
     {
         label: 'upCRO',
         value: 1,
         image: '/img/upcro_coin_ic.svg',
     }
 ];
-const currencytwo = [
-{
-    label: 'BTC',
-    value: 0,
-    image: '/img/btc_coin_ic.svg',
-},
-{
-    label: 'BTC',
-    value: 1,
-    image: '/img/btc_coin_ic.svg',
-}
-];
   
 
 export default function Swap() {
+    const { account, library, chainId } = useWeb3React();
+    const [ inputValue, setInputValue] = useState<string>("");
+    const [ outputValue, setOutputValue] = useState<string>("");
+    const [ currencyInput, setCurrenyInput] = useState<any>(currency);
+    const [ currencyOutput, setCurrenyOutput]  = useState<any>(currencytwo);
+    const [balance, setBalance] = useState(0)
+    const [price, setPrice] = useState("0")
+    const [rootedBalance, setRootedBalance] = useState(0)
+
+    const fetchBalance = async () => {
+        const balance = await library.getBalance(account!)
+        setBalance(balance.toString())      
+    }    
+
+    const fetchRootedBalance = async () => {
+        const service = new TokenService(library, account!, ROOTED_ADDRESS)
+        const balance = await service.getBalance(account!)
+        setRootedBalance(balance.toString())      
+    }    
+
+    useEffect(() => {
+        if (account && library && chainId) {
+            fetchBalance()
+            fetchRootedBalance()
+        }
+    }, [account, chainId, library, fetchBalance, setBalance])
+
+    const doSwap = async() => {
+        const swapService = new SwapService(library, account!)
+        await swapService.swap(inputValue, outputValue, 
+            (currencyInput[0].value == 0 ? BASE_ADDRESS : ROOTED_ADDRESS),
+            (currencyInput[0].value == 0 ? ROOTED_ADDRESS : BASE_ADDRESS)
+        )
+    }
+
+    const onInputChange = async(event: React.ChangeEvent<HTMLInputElement>) => {        
+        if (!isValid(event.target.value)) return  
+        const newValue = event.target.value.replace(/,/g, '.')
+        inputChange(newValue, currencyInput[0].value)
+    }
+
+    const inputChange = async(newValue: string, currentInputValue: any) => {
+        setInputValue(newValue)       
+        
+        if(newValue != ""){
+            const swapService = new SwapService(library, account!)
+
+            if(currentInputValue == 0){
+                const amount = await swapService.getAmountOut(newValue)
+                const outputVal = ethers.utils.formatEther(amount.toString()).toString()
+                const priceData = (parseFloat(outputVal) / parseFloat(newValue)).toFixed(3)
+                setOutputValue(parseFloat(outputVal).toFixed(3).toString())
+                setPrice(parseFloat(priceData).toFixed(3))
+            }
+            else{
+                const amount = await swapService.getAmountOut2(newValue)
+                const outputVal = ethers.utils.formatEther(amount.toString()).toString()
+                const priceData = (parseFloat(outputVal) / parseFloat(newValue)).toFixed(8)
+                setOutputValue(parseFloat(outputVal).toFixed(8).toString())
+                setPrice(parseFloat(priceData).toFixed(8))
+            }
+        }
+        else{
+            setOutputValue("")
+            setPrice("0")
+        }
+    }
+
+    const onInputSwap = async() => {
+        setOutputValue("")
+        if(currencyInput[0].value == 0){
+            setCurrenyInput(currencytwo)
+            setCurrenyOutput(currency)
+            inputChange(outputValue, 1)
+        }
+        else{
+            setCurrenyInput(currency)
+            setCurrenyOutput(currencytwo)
+            inputChange(outputValue, 0)
+        }
+    }
+
+    const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`) // match escaped "." characters via in a non-capturing group
+    const isValid = (value: string) => value === '' || inputRegex.test(escapeRegExp(value))
+
   return (
     <>
         <LayoutTwo>
@@ -40,13 +123,14 @@ export default function Swap() {
                         <Box className='spwa_cntnt_dark_box'>
                             <Box className='swap_upcro_brdr_bx'>
                                 <Box className='flex_bx'>
-                                    <input type="number" value="281.594" />
+                                    <input type="number" value={inputValue} 
+                                    onChange={event => onInputChange(event)} />
                                     <Box className='slect_box'>
                                         <ReactSelect
                                             className='select_one'
                                             classNamePrefix="slct"
-                                            value={currency}
-                                            options={currency}
+                                            value={currencyInput[0]}
+                                            options={currencyInput}
                                             formatOptionLabel={currency => (
                                             <Box className='slect_bg'>
                                             <div className="country-option slect_bg">
@@ -59,20 +143,27 @@ export default function Swap() {
                                     </Box>
                                 </Box>
                                 <Box className='flex_bx flex_bx_02'>
-                                    <Text>$281.594</Text>
-                                    <Text>Balance: 0.00</Text>
+                                    <Text></Text> 
+                                    <Text>Balance: {currencyInput != null &&  currencyInput[0].value == 0 ? 
+                                    getDisplayBalance(new BigNumber(balance), 4, BASE_DECIMALS) : 
+                                    getDisplayBalance(new BigNumber(rootedBalance), 4, BASE_DECIMALS)}</Text>
                                 </Box>
                             </Box>
-                            <Button className='swap_center_btn'><Image src='img/down_arw_ic.svg' /></Button>
+                            <Button className='swap_center_btn' onClick={e => onInputSwap()}><Image src='img/down_arw_ic.svg' /></Button>
                             <Box className='swap_upcro_brdr_bx swap_btc_brdr_bx'>
                                 <Box className='flex_bx'>
-                                    <input type="number" value="3655.548654" />
+                                    <input type="number" value={outputValue} 
+                                        onChange={event => {
+                                        if (!isValid(event.target.value)) return  
+                                        const newValue = event.target.value.replace(/,/g, '.')
+                                        setOutputValue(newValue)
+                                        }} />
                                     <Box className='slect_box'>
                                         <ReactSelect
                                             className='select_one'
                                             classNamePrefix="slct"
-                                            value={currencytwo}
-                                            options={currencytwo}
+                                            value={currencyOutput[0]}
+                                            options={currencyOutput}
                                             formatOptionLabel={currencytwo => (
                                             <div className="country-option slect_bg">
                                                 <img src={currencytwo.image} alt="country-image" />
@@ -83,23 +174,25 @@ export default function Swap() {
                                     </Box>      
                                 </Box>
                                 <Box className='flex_bx flex_bx_02'>
-                                    <Text>$256.312</Text>
-                                    <Text>Balance: 1,688,648</Text>
+                                    <Text></Text>
+                                    <Text>Balance: {currencyInput != null &&  currencyInput[0].value == 1 ? 
+                                    getDisplayBalance(new BigNumber(balance), 4, BASE_DECIMALS) : 
+                                    getDisplayBalance(new BigNumber(rootedBalance), 4, BASE_DECIMALS)}</Text>
                                 </Box>
                             </Box>
                             <Box className='text_row_darc'>
-                                <Text>1 upCRO = 0.00846454 BTC</Text>
-                                <Text>($0.00065486635)</Text>
-                                <Text>Gas: $32.455</Text>
+                                <Text>1 {currencyInput[0].label} = {price} {currencyOutput[0].label}</Text>
+                                {/* <Text>($0.00065486635)</Text>
+                                <Text>Gas: $32.455</Text> */}
                             </Box>
-                            <Button className='stake_full_btn'>Stake</Button>
+                            <Button className='stake_full_btn' onClick={e => doSwap()}>Swap</Button>
                         </Box>
                     </Box>
                     <Box className='contracts_box'>
                         <Heading as="h4">Contracts</Heading>
                         <Box className='upcro_copyflex'>
-                            <Heading as="h6">upCRO<Text>0xb0620........f790f<Button><Image src="img/copy_ic.svg" alt='' /></Button></Text></Heading>
-                            <Heading as="h6" className='right_h6'>xUpCRO<Text>0x78Bf85......ed90e<Button><Image src="img/copy_ic.svg" alt='' /></Button></Text></Heading>
+                            <Heading as="h6">upCRO<Text>{shortenAddress(ROOTED_ADDRESS)}<Button><Image src="img/copy_ic.svg" alt='' /></Button></Text></Heading>
+                            <Heading as="h6" className='right_h6'>xUpCRO<Text>{shortenAddress(STAKING_ADDRESS)}<Button><Image src="img/copy_ic.svg" alt='' /></Button></Text></Heading>
                         </Box>
                     </Box>
                 </Container>
