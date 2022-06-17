64aa02ad262aac4e2e2e6d0092582fba424d47cd
ravirasadiya <ravirasadiya1994@gmail.com>
2022-06-17 12:37:23 +0530

    gangis

--- src/components/Home/FirstSec.tsx
+++ src/components/Home/FirstSec.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Box, SimpleGrid, GridItem, Heading, Text, Button, Image } from '@chakra-ui/react'
+import { Box, SimpleGrid, GridItem, Heading, Text, Button,  } from '@chakra-ui/react'
 import { Element } from 'react-scroll';
+import Lottie from 'react-lottie'
+import t1 from '../../assets/lottie/upCRO.json'
 // import { motion, useMotionValue, useTransform } from "framer-motion";
 
 export default function FirstSec() {
@@ -16,6 +18,14 @@ export default function FirstSec() {
     //     x.set(event.clientX - rect.left);
     //     y.set(event.clientY - rect.top);
     // }
+    const top = {
+        loop: true,
+        autoplay: true,
+        animationData: t1,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
   return (
     <>
@@ -58,7 +68,9 @@ export default function FirstSec() {
                                         rotateY: rotateY
                                     }}> */}
                                         <Box className='radial_gradiant_box'>
-                                            <Image src='img/first_clas_img_one.svg' className='upcro_first_img' />
+                                             <Lottie options={top} width={450} height={450}></Lottie>
+
+                                            {/* <Image src='img/first_clas_img_one.svg' className='upcro_first_img' /> */}
                                         </Box>
                                     {/* </motion.div>
                                 </motion.div> */}
